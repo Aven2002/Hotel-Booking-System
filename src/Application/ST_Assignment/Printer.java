4506901325b02c66ce7a3fa0399912fcba14407d
domo536 <domo_536@yahoo.com.my>
2024-04-21 04:22:41 +0800

    Some progress

--- src/Application/ST_Assignment/Printer.java
+++ src/Application/ST_Assignment/Printer.java
@@ -1,5 +1,15 @@
 package ST_Assignment;
 
 public class Printer {
+	
+	public void printInfo(String name, String member_type, String room_type) {
+		System.out.println("\n==============================================");
+        System.out.println("                   Result                     ");
+        System.out.println("==============================================\n");
+        System.out.println("        Name          : " + name);
+        System.out.println("        Member Type   : " + member_type);
+        System.out.println("        Room Type     : " + room_type);
+        System.out.println("\n==============================================");
 
+	}
 }
--- src/Application/ST_Assignment/WaitingList.java
+++ src/Application/ST_Assignment/WaitingList.java
@@ -1,5 +1,40 @@
 package ST_Assignment;
 
-public class WaitingList {
+import java.util.*;
 
+public class WaitingList {
+	ArrayList<String> VIP = new ArrayList<String>();
+	ArrayList<String> member = new ArrayList<String>();
+	ArrayList<String> normal = new ArrayList<String>();
+	
+	String memberType;
+	//Booking booking = new Booking();
+	User user = new User();
+	int userID;
+	
+	
+	public void addWaiting() {
+		
+	}
+	
+	public void getWaiting() {
+		userID = user.getUserID(memberType);
+		
+	}
+	
+	public void removeWaiting() {
+		
+	}
+	
+
+	public String getMemberType() {
+		return memberType;
+	}
+
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+	
+	
+	
 }
